20dc22620d2ee82f4934ce5d3da03e5c4fc72ec3
Ruta Jogani <ruta007jogani@gmail.com>
2025-06-05 22:53:29 +0530

    add: convert decimal to binary file for html and js

---
+++ binary.js
@@ -0,0 +1,36 @@
+function convertDecimalToBinary(number) {
+  if (number === 0) {
+    return 0;
+  }
+
+  let numberOfBits = 0;
+  for (let i = 0; i < number; i++) {
+    const value = 2 ** i;
+    if (number < value) {
+      numberOfBits = i - 1;
+      break;
+    }
+  }
+
+  let total = 0;
+  let binary = "";
+
+  for (let i = numberOfBits; i >= 0; i--) {
+    const preValue = 2 ** i;
+    total += preValue;
+    if (total <= number) {
+      binary += "1";
+    } else {
+      binary += "0";
+      total -= preValue;
+    }
+  }
+  return binary;
+}
+const input = document.getElementById("input");
+
+const submit = document.getElementById("submit");
+submit.addEventListener("click", () => {
+  const display_1 = document.getElementById("display");
+  display_1.innerHTML = convertDecimalToBinary(input.value);
+});
